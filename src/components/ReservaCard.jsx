cecc93bedcbd824a48d2cb3c0f9d9755b21043c4
FabricioDiazC <111374415+FabricioDiazC@users.noreply.github.com>
2026-05-05 02:17:56 -0300

    Fix Validacion de seguridad para slice de hora

--- src/components/ReservaCard.jsx
+++ src/components/ReservaCard.jsx
@@ -1,7 +1,6 @@
 import { FaClock, FaUsers, FaEdit, FaTrash } from 'react-icons/fa';
 
-export default function ReservaCard({ reserva, onDelete, onEdit}) {
-  // Función para dar formato a la fecha
+export default function ReservaCard({ reserva, onDelete, onEdit }) {
   const formatFecha = (fechaStr) => {
     const opciones = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
     return new Date(fechaStr + 'T00:00:00').toLocaleDateString('es-ES', opciones);
@@ -20,7 +19,9 @@ export default function ReservaCard({ reserva, onDelete, onEdit}) {
         <div className="text-bar-muted text-sm space-y-2 mt-4">
           <p className="flex items-center gap-2">
             <FaClock className="text-zinc-500"/> 
-            {formatFecha(reserva.fecha)} • {reserva.hora_inicio.slice(0,5)} a {reserva.hora_fin.slice(0,5)}
+            {formatFecha(reserva.fecha)} • 
+            {reserva.hora_inicio?.slice(0,5)} 
+            {reserva.hora_fin ? ` a ${reserva.hora_fin.slice(0,5)}` : ''}
           </p>
           <p className="flex items-center gap-2">
             <FaUsers className="text-zinc-500"/> 
@@ -30,13 +31,11 @@ export default function ReservaCard({ reserva, onDelete, onEdit}) {
       </div>
 
       <div className="mt-6 pt-4 border-t border-zinc-800 flex justify-end gap-3">
-        {/* Agregar despues logica para editar loco */}
-        <button onClick={() => onEdit(reserva)}
-        className="text-zinc-400 hover:text-white transition">
-          <FaEdit />
+        <button onClick={() => onEdit(reserva)} className="text-zinc-400 hover:text-white transition cursor-pointer" title="Editar">
+          <FaEdit size={18} />
         </button>
-        <button onClick={() => onDelete(reserva.id)} className="text-red-500 hover:text-red-400 transition" title="Eliminar">
-          <FaTrash />
+        <button onClick={() => onDelete(reserva.id)} className="text-red-500 hover:text-red-400 transition cursor-pointer" title="Eliminar">
+          <FaTrash size={18} />
         </button>
       </div>
     </div>
